3a1df5a5a033afee9b6d8814108ca4677285f7fd
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-07-18 13:51:13 -0500

    added images to product page

---
+++ scripts.js
@@ -0,0 +1,49 @@
+console.log("hello world");
+
+
+
+function show() {
+  document.getElementById('div1').innerHTML="<a href='#'><img src='img/ayeAye.jpg'></a>";
+};
+
+show();
+
+function show2() {
+  document.getElementById('div2').innerHTML="<a href='#'><img src='img/littleImp.jpg'></a>";
+};
+
+show2();
+
+function show3() {
+  document.getElementById('div3').innerHTML="<a href='#'>img src='img/nicobarBird.jpg'></a>";
+};
+
+show3();
+
+function show4 () {
+  document.getElementById('div4').innerHTML="<a href='#'> img src='img/slowLoris.jpg'></a>";
+};
+
+show4();
+
+// var div1 = document.getElementById("div1");
+// div1 = "img src='img/ayeAye.jpg";
+
+// var div2 = document.getElementById("div2");
+// div2 = "<p>Hello</p>";
+// //document.getElementById("div1").innerHTML = "<h2> The Aye Aye</h2>";
+
+// //document.getElementById('div2').innerHTML = ""
+
+// console.log('hello');
+
+
+
+
+
+
+
+
+
+
+
